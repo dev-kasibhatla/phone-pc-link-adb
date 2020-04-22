f285277d97f8442e5e5747d51aab5c7f2eeee278
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2020-04-22 19:44:31 +0530

    manage all communication

---
+++ AdbManager.py
@@ -0,0 +1,27 @@
+from subprocess import Popen, PIPE
+import Commands as com
+
+def e(action):
+    global process
+    print(action)
+    process.stdin.write(('input '+action+'\n').encode())
+    process.stdin.flush()
+    return True
+    #print(repr(process.stdout.readline())) # Should print output
+
+def dot(action):
+    e(com.getTap(action))
+
+def dos(action):
+    e(com.getSwipe(action))
+
+
+def closeShell():
+    global process
+    process.stdin.close()
+    print('Waiting for adb shell to exit')
+    process.wait()
+    print('adb shell finished with return code %d' % process.returncode)
+
+process = Popen(['adb shell\n'],shell=True, stdin=PIPE, stdout=PIPE)
+print("Process initialized")
