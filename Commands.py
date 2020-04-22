29d486f5549dda577620398b537fc70bf72b33fc
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2020-04-22 17:53:28 +0530

    generate action script

---
+++ Commands.py
@@ -0,0 +1,20 @@
+import DataManager as D
+tapDelay = 50
+def init():
+    data = D.DM()
+    data.getTapData()
+    data.getSwipeData()
+    #print("Complete")
+
+def getTap(action):
+    #search for name and return
+    for tap in D.taps:
+        if tap.name == action:
+            return "swipe "+str(tap.x)+' '+str(tap.y)+ ' ' + str(tap.x)+' '+str(tap.y) + ' ' + str(tapDelay)
+    print("Can't find "+action)
+
+def getSwipe(action):
+    for swipe in D.swipes:
+        if swipe.name == action:
+            return "swipe "+str(swipe.x1)+' '+str(swipe.y1)+ ' ' + str(swipe.x2)+' '+str(swipe.y2) + ' ' + str(tapDelay*2)
+    print("Can't find "+action)
